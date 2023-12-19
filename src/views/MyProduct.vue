ffae93fe6a15f281fb30b582b6bebe864c5329ae
Andyeeeee <10514f074@gmail.com>
2023-12-19 17:08:52 +0800

    imagURL

--- src/views/MyProduct.vue
+++ src/views/MyProduct.vue
@@ -20,11 +20,11 @@ import { computed } from 'vue';
 
 const items = computed(() => {
   return [
-    { href: 'https://andyeeeee.github.io/vuetify-project1/#/', image: '../assets/Product.png', title: '餐廳線上訂餐', text: '餐廳線上訂餐' },
-    { href: 'https://andyeeeee.github.io/tomato/#/', image: '@/assets/TomatoClock.png', title: '番茄中', text: '番茄中' },
-    { href: 'https://andyeeeee.github.io/FIGHT-mosquito/', image: 'src/assets/Jq.png', title: 'JQ蚊子', text: 'JQ蚊子' },
-    { href: 'https://andyeeeee.github.io/RWD05/', image: 'src/assets/RWDrestran.png', title: '餐廳面板RWD', text: '餐廳面板RWD' },
-    { href: '', image: 'src/assets/LineBotApi.png', title: 'LineBotApi', text: 'LineBotApi' }
+    { href: 'https://andyeeeee.github.io/vuetify-project1/#/', image: 'https://www.cakeresume.com/cdn-cgi/image/fit=scale-down,format=auto,w=828/https://images.cakeresume.com/20RdA/10514f074/06cc4bc1-2c41-4f23-91b6-51f53c430a1c.png', title: '餐廳線上訂餐', text: '餐廳線上訂餐' },
+    { href: 'https://andyeeeee.github.io/tomato/#/', image: 'https://www.cakeresume.com/cdn-cgi/image/fit=scale-down,format=auto,w=828/https://images.cakeresume.com/20RdA/10514f074/5c7774f2-3c63-47f6-8008-3609ce41ce18.png', title: '番茄中', text: '番茄中' },
+    { href: 'https://andyeeeee.github.io/FIGHT-mosquito/', image: 'https://www.cakeresume.com/cdn-cgi/image/fit=scale-down,format=auto,w=828/https://images.cakeresume.com/20RdA/10514f074/9cd2e7fb-1038-469a-bab9-a420319696a4.png', title: 'JQ蚊子', text: 'JQ蚊子' },
+    { href: 'https://andyeeeee.github.io/RWD05/', image: 'https://www.cakeresume.com/cdn-cgi/image/fit=scale-down,format=auto,w=828/https://images.cakeresume.com/20RdA/10514f074/64a0791a-1d3b-4bf9-8ed8-17f98957db48.png', title: '餐廳面板RWD', text: '餐廳面板RWD' },
+    { href: '', image: 'https://www.cakeresume.com/cdn-cgi/image/fit=scale-down,format=auto,w=828/https://images.cakeresume.com/20RdA/10514f074/acee3368-5a45-4aba-a0bf-ecff0605b010.png', title: 'LineBotApi', text: 'LineBotApi' }
   ]
 })
 </script>
